d99a8f0ee3410374f2e2c0e6c151e8098627339b
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-10 21:38:50 +0530

    Added : Emp Attendence Program

--- Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
+++ Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
@@ -3,5 +3,15 @@ package com.bridgelabz.empwage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
+        int isFullTime=1;
+        double empCheck = Math.floor(Math.random()*10)%2;
+        if(empCheck==isFullTime)
+        {
+            System.out.println("Employee is Present..");
+        }
+        else {
+            System.out.println("Employee is Abscent");
+        }
+
     }
 }
