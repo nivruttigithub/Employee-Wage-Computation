7db5ed3058a12f2a2b127e1cb04024984a75f13c
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-10 21:50:49 +0530

    Added : Calculate Emp_Wage Problem

--- Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
+++ Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
@@ -4,14 +4,20 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Problem");
         int isFullTime=1;
+        int empRatePerHour = 20;
+        int empHrs=0;
+        int empWage=0;
         double empCheck = Math.floor(Math.random()*10)%2;
         if(empCheck==isFullTime)
         {
             System.out.println("Employee is Present..");
+            empHrs=8;
         }
         else {
             System.out.println("Employee is Abscent");
+            empHrs=0;
         }
-
+        empWage = empHrs*empRatePerHour;
+        System.out.println("Employee wage: "+empWage);
     }
 }
