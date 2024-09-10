e1d170422ec57d7bd99a2400c8c512baf1f52a5b
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-10 22:18:07 +0530

    Added : Calc Wages Using Switch Msg

--- Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
+++ Employee-Wage-Computation/src/main/java/com/bridgelabz/empwage/EmployeeWage.java
@@ -1,27 +1,24 @@
 package com.bridgelabz.empwage;
 
 public class EmployeeWage {
+    public static final int isPartTime = 1;
+    public static final int isFullTime = 2;
+    public static final int empRatePerHour =20;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Problem");
-        int isPartTime=1;
-        int isFullTime=2;
-        int empRatePerHour = 20;
+
         int empHrs=0;
         int empWage=0;
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if(empCheck==isFullTime)
+        int  empCheck = (int) Math.floor(Math.random()*10)%3;
+        switch (empCheck)
         {
-            System.out.println("Employee is Present Full Time..");
-            empHrs=8;
-        }
-        else if(empCheck==isPartTime)
-        {
-            System.out.println("Employee is Present Part Time..");
-            empHrs=4;
-        }
-        else {
-            System.out.println("Employee is Abscent");
-            empHrs=0;
+            case isPartTime:
+             empHrs = 4;
+             break;
+            case isFullTime:
+              empHrs=8;
+              break;
+            default:
+              empHrs = 0;
         }
         empWage = empHrs*empRatePerHour;
         System.out.println("Employee wage: "+empWage);
